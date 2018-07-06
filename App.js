b0126c7dc14847ef68614e80da79fca1cbb3dce6
dony <dony@wobb.co>
2018-07-06 12:04:11 +0800

    Initialize Firebase App

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {
 import { Container } from 'native-base';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import Head from './src/components/Head';
 import reducers from './src/reducers';
 
@@ -24,6 +25,17 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAAll6BB23dC4xycq7Y-ZEYHUk5cJ-OTH4',
+      authDomain: 'empmanager-d5408.firebaseapp.com',
+      databaseURL: 'https://empmanager-d5408.firebaseio.com',
+      projectId: 'empmanager-d5408',
+      storageBucket: 'empmanager-d5408.appspot.com',
+      messagingSenderId: '417312587608'
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
