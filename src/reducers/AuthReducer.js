e34067d356a78d16951dc78e4416f51b6b043a6d
dony <dony@wobb.co>
2018-07-06 15:54:34 +0800

    Add action type.js

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,7 +1,11 @@
+import { EMAIL_CHANGED } from '../actions/type';
+
 const INITIAL_STATE = { email: '' };
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
+		case EMAIL_CHANGED:
+			break;
 		default:
 			return state;
 	}
