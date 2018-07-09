0d72f7985600e33becaff2426c05035d411172a8
dony <dony@wobb.co>
2018-07-09 15:09:59 +0800

    Create EmployeeList Scene

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
 		<Router>
-			<Scene key="root">
-				<Scene key="login" component={LoginForm} title="Please login" hideNavBar />
+			<Scene key="root" hideNavBar>
+				<Scene key="auth">
+					<Scene key="login" component={LoginForm} title="Please login" hideNavBar />
+				</Scene>
+				<Scene key="main" initial>
+					<Scene 
+					rightTitle=" Add"
+					onRight={() => console.log('Right!')}
+					key="employeeList" 
+					component={EmployeeList} 
+					title="Employee List" 
+					
+					/>
+				</Scene>
 			</Scene>
 		</Router>
 	);
