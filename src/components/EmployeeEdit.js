669f6cab790d76d8b353b4c514156e1c227f9259
dony <dony@wobb.co>
2018-07-10 17:30:57 +0800

    Add Text Message function

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card, CardItem, Spinner, Button, Title } from 'native-base';
+import { text } from 'react-native-communications';
+import { Card, CardItem, Spinner, Button, Title, View } from 'native-base';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
 
@@ -17,15 +18,42 @@ class EmployeeEdit extends Component {
 		this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
 	}
 
+	onTextPress() {
+		const	{ phone, shift } = this.props;
+		text(phone, `Your schedule is on ${shift}`);
+	}
+
+	onFirePress() {
+		
+	}
+
 	renderButton() {
 		if (this.props.loading) {
-			return <Spinner color='green' style={{ justifyContent: 'center', flex: 1 }} />;
+			return (
+				<CardItem>
+					<Spinner color='green' style={{ justifyContent: 'center', flex: 1 }} />
+				</CardItem>
+			);
 		}
 
 		return (
-			<Button block style={{ flex: 1 }} onPress={this.onButtonSave.bind(this)}>
-        <Title>Save</Title>
-      </Button>
+			<View>
+				<CardItem>
+					<Button block style={{ flex: 1 }} onPress={this.onButtonSave.bind(this)}>
+						<Title>Save</Title>
+					</Button>
+				</CardItem>
+				<CardItem>
+					<Button block style={{ flex: 1 }} onPress={this.onTextPress.bind(this)}>
+						<Title>Text Schedule</Title>
+					</Button>
+				</CardItem>
+				<CardItem>
+					<Button block style={{ flex: 1 }} onPress={this.onFirePress.bind(this)}>
+						<Title>Fire</Title>
+					</Button>
+				</CardItem>
+			</View>
 		);
 	}
 
@@ -33,9 +61,7 @@ class EmployeeEdit extends Component {
     return (
       <Card style={{ flex: 0, paddingBottom: 10 }}>
         <EmployeeForm {...this.props} />
-        <CardItem>
-          {this.renderButton()}
-        </CardItem>
+        {this.renderButton()}
       </Card>
     );
   }
